1cd965574f5ed65b137d61668a633e74835ee2a1
bizayprasadmandal <81038994+bizayprasadmandal@users.noreply.github.com>
2023-11-30 22:40:40 +0545

    basics operations like creation, read, update and delete has been performed in this section where products items are created, edited, updated and deleted

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
+Route::put('/product/{product}/update', [ProductController::class, 'update'])->name('product.update');
+Route::delete('/product/{product}/delete', [ProductController::class, 'delete'])->name('product.delete');
+
+
+
